974ac4100d6905235f27f05ac739989a45300d4b
Seb-Leb <seb@leb.cat>
2020-12-15 09:13:56 -0500

    building snpeff pipeline..

--- openvar.py
+++ openvar.py
@@ -2,23 +2,30 @@ import os
 import subprocess
 
 class VCF:
-	def __init__(self, data_dir, file_name):
-		self.data_dir  = data_dir
-		self.file_name = file_name
-		self.file_path = os.path.join(data_dir, file_name)
+	def __init__(self, data_dir, file_name, study_name):
+		self.data_dir   = data_dir
+		self.file_name  = file_name
+		self.file_path  = os.path.join(data_dir, file_name)
+		self.study_name = study_name
 
-	def sanitize(self, ):
+	def check(self, ):
 		# check genome version - convert?
 		# check ref-alt allele order GNOMAD
 		# check 'chr' in chrome names
 
 		# return proper feedback in case of non-compliance
+		pass
 
-	def split_per_chrom(self, ):
-		# may not be necessary
+	def check_alt_ref_alleles(self, ):
+		pass
+		
+	def convert_hg19_to_hg38(self, ):
+		pass
 
 	def store(self, ):
-		# store sanitized vcf in db 
+		# store sanitized vcf in db
+		pass
+
 
 class OpenVar:
 	def __init__(self, snpeff_path, vcf, genome_build):
@@ -27,9 +34,10 @@ class OpenVar:
 		self.snpeff_build = 'GRCh38.95_refAlt'
 		self.vcf = vcf
 
-	def run_snpeff_pipe(self, ):
+	def run_snpeff_pipe(self):
+		snpEff_logfile = os.path.join(vcf.data_dir, '{}_snpEff.log'.format(vcf.study_name))
 		snpeff_cmd  = self.get_snpeff_cmd()
-		snpeff_subproc = subprocess.Popen(snpeff_cmd.split(), shell=False, stdout=open(, 'w'))
+		snpeff_subproc = subprocess.Popen(snpeff_cmd.split(), shell=False, stdout=open(snpEff_logfile, 'w'))
 		snpeff_subproc.wait()
 
 		cat_cmd  = self.get_cat_cmd()
@@ -39,12 +47,13 @@ class OpenVar:
 		cat_subproc.stdout.close()
 
 		snpsift_cmd  = self.get_snpsift_cmd()
-		snpsift_subproc = subprocess.Popen(snpsift_cmd, shell=True, stdin=perl_subproc.stdout, stdout=open(chr_cmds[chrom]['annOnePerLine'], "w"))
+		annOnePerLine_file = os.path.join(vcf.data_dir, '{}_annOnePerLine.tsv'.format(vcf.study_name))
+		snpsift_subproc = subprocess.Popen(snpsift_cmd, shell=True, stdin=perl_subproc.stdout, stdout=open(annOnePerLine_file, "w"))
 
 	    perl_subproc.stdout.close()
 	    snpsift_subproc.wait()		
 
-	def get_snpeff_cmd(self, ):
+	def get_snpeff_cmd(self):
 		cmd = 'java -Xmx12g -jar {snpeff_jar} -v {build} {vcf_path}'.format(
 				snpeff_jar = self.snpeff_jar,
 				build = self.snpeff_build,
@@ -52,15 +61,15 @@ class OpenVar:
 			)
 		return cmd
 
-	def get_cat_cmd(self, ):
+	def get_cat_cmd(self):
 		cmd = 'cat {output_vcf}'.format(output_vcf=self.vcf.file_name.replace('.vcf', '.ann.vcf'))
 		return cmd
 
-	def get_perl_cmd(self, ):
+	def get_perl_cmd(self):
 		cmd = os.path.join(self.snpeff_path, 'scripts', 'vcfEffOnePerLine.pl')
 		return cmd
 
-	def get_snpsift_cmd(self, ):
+	def get_snpsift_cmd(self):
 		snpsift_fields = 'CHROM POS REF ALT "ANN[*].EFFECT" "ANN[*].IMPACT" "ANN[*].HGVS_P" "ANN[*].FEATUREID" "ANN[*].GENE" "ANN[*].ERRORS" "ANN[*].HGVS_C"'
 		cmd ='java -jar {snpsift_jar} extractFields - {snpsift_fields}'.format(
 				snpsift_jar = self.snpsift_jar,
@@ -69,10 +78,12 @@ class OpenVar:
 		return cmd
 
 
-
 class OPVReport:
 	def __init__(self, opv):
+		pass
 
 	def write_tabular(self, ):
+		pass
 
 	def write_html(self, ):
+		pass
