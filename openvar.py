697316785dc547665034a27f063953f77615574f
MAB-Lab <marie.brunet@usherbrooke.ca>
2021-05-20 12:41:22 -0400

    fix var names

--- openvar.py
+++ openvar.py
@@ -652,7 +652,6 @@ class OPVReport:
                     var_name = '_'.join([line['CHROM'], line['POS'], line['REF'], line['ALT'], line['ANN[*].GENE']])
                 else:
                     var_name = '_'.join([line['CHROM'], line['POS'], line['REF'], line['ALT'], 'NA'])
-                var_name = '_'.join([line['CHROM'], line['POS'], line['REF'], line['ALT'], line['ANN[*].GENE']])
                 eff = (var_name, *[line['ANN[*].' + x] if 'ANN[*].' + x in line else 'NA' for x in fields])
                 if as_dict:
                     yield line
